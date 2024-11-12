9ea17e92fd3fca64d4f668785f917d1117497e3e
S-Pz <saviofrancisco150@aluno.ufsj.edu.br>
2024-11-12 14:45:27 -0300

    Adding functions about clans

--- src/api.py
+++ src/api.py
@@ -47,6 +47,86 @@ def search_for_clans(token, clan_tag):
 
     return json_resul
 
+def search_for_clan_war_log(token, clan_tag):
+    
+    encoded_clan_tag = quote(clan_tag) if clan_tag else ""
+    url = "https://api.clashofclans.com/v1/clans/" + encoded_clan_tag + "/warlog"
+    header = get_auth_header(token)
+
+    result = requests.get(url, headers=header)
+
+    json_resul = json.loads(result.content)
+
+    if(len(json_resul) == 0):
+        print("No clan war founded")
+        return None
+    
+    return json_resul
+
+def information_about_war(token, war_tag):
+
+    encoded_war_tag = quote(war_tag) if war_tag else ""
+    url = "https://api.clashofclans.com/v1/clanwarleagues/wars/" + encoded_war_tag
+    header = get_auth_header(token)
+
+    result = requests.get(url, headers=header)
+
+    json_result = json.loads(result.content)
+
+    if(len(json,result) == 0):
+        print("No clan warleague founded")
+        return None
+
+    return json_result
+
+def information_about_currentwar_leaguegroup(token, clan_tag):
+
+    encoded_clan_tag = quote(clan_tag) if clan_tag else ""
+    url = "https://api.clashofclans.com/vi/clans/" + encoded_clan_tag + "/currentwar/leaguegroup"
+    header = get_auth_header(token)
+
+    result = requests.get(url, headers=header)
+
+    json_result = json.loads(result.content)
+
+    if(len(json,result) == 0):
+        print("No current war founded")
+        return None
+    
+    return json_result
+
+def clan_currentwar(token, clan_tag):
+
+    encoded_clan_tag = quote(clan_tag) if clan_tag else ""
+    url = "https://api.clashofclans.com/vi/clans" + encoded_clan_tag + "/currentwar"
+    hearder = get_auth_header(token)
+
+    result = requests.get(url, headers=hearder)
+
+    json_resul = json.loads(result.content)
+
+    if(len(json, result) == 0):
+        print("No current war founded")
+        return None
+    
+    return json_resul
+
+def clan_capitalraid(token, clan_tag):
+
+    encoded_clan_tag = quote(clan_tag) if clan_tag else ""
+    url = "https://api.clashofclans.com/vi/clans" + encoded_clan_tag + "/capitalraidseasons"
+    header = get_auth_header(token)
+
+    result = requests.get(url, headers=header)
+
+    json_resul = json.loads(resutl.content)
+
+    if(len(json, result) == 0):
+        print("No clan season founded")
+        return None
+    
+    return json_resul
+
 if __name__ == '__main__':
 
     player_tag = "#U8RRR8LY"
@@ -70,3 +150,4 @@ if __name__ == '__main__':
 
     with open(f"clan{clan_tag}Information.json","w", encoding='utf-8') as f:
         json.dump(result, f, ensure_ascii=False, indent=4)
+    
