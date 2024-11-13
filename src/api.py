d4a0f6ea2e918b67fea5be741102fb154422034d
S-Pz <saviofrancisco150@aluno.ufsj.edu.br>
2024-11-13 17:34:21 -0300

    commiting some code

--- src/api.py
+++ src/api.py
@@ -157,21 +157,55 @@ if __name__ == '__main__':
     # # with open(f"player{player_tag}Information.json","w", encoding='utf-8') as f:
     # #     json.dump(result, f, ensure_ascii=False, indent=4)
 
-    # result = search_for_clans(API_TOKEN, clan_tag)
-    
-    # for a in result['memberList']:
-        
-    #     player_tag = a['tag']
-
-    #     result = searh_for_player(API_TOKEN,player_tag)
-
-    #     with open(f"player{player_tag}Information.json","w", encoding='utf-8') as f:
-    #         json.dump(result, f, ensure_ascii=False, indent=4)
-
     # with open(f"clan{clan_tag}Information.json","w", encoding='utf-8') as f:
     #     json.dump(result, f, ensure_ascii=False, indent=4)
-
-    result= search_clan(API_TOKEN)
     
-    with open("clans.json","w", encoding='utf-8') as f:
-        json.dump(result, f, ensure_ascii=False, indent=4)
+    clans_info:list=[]
+
+    clans = search_clan(API_TOKEN)
+    
+    for info in clans['items']:
+        
+        clans_data = {
+
+            'tag': info['tag'],
+            'name':info['name'],
+            'location':info['location']['countryCode'],
+            'clanLevel':info['clanLevel'],
+            'warFrequency':info['warFrequency'],
+            'warFrequency': info['warFrequency'],
+            'warWinStreak': info['warWinStreak'],
+            'warWins': info['warWins'],
+            'members': info['members'],
+        }
+        clans_info.append(clans_data)
+
+        player = search_for_clans(API_TOKEN, clans_data['tag'])
+
+        for a in player['memberList']:
+        
+            player_tag = a['tag']
+            player_info = searh_for_player(API_TOKEN,player_tag)
+
+            for player in player_info:
+
+                player_data = {
+                    'tag': player['tag'],
+                    'name':player['name'],
+                    'townHallLevel':player['townHallLevel'],
+                    'warStars':player['warStars'],
+                    'attackWins':player['attackWins'],
+                    'defenseWins': player['defenseWins'],
+                    'donations': player['donations'],
+                    'donationsReceived': player['donationsReceived'],
+                    'clan_tag': player['clan']['tag'],
+                    'clan_name':player['clan']['name']
+                }
+
+    with open("clans_info.json","w", encoding='utf-8') as f:
+        json.dump(clans_info, f, ensure_ascii=False, indent=4)
+    
+    
+    
+    # 
+    
